eb1ca6485f22203b2a7f7e1167b1c3ad27ca7a90
ianno <puramriano2002@gmail.com>
2026-05-26 16:37:37 +0800

    added migration fix on server

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
         $publicUploadRoot = config('filesystems.disks.public.root');
 
         File::ensureDirectoryExists($publicUploadRoot);
